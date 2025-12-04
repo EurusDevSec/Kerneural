d551d65dee2468ca6b805d8ee48b96319a0bc462
LeganSar44 <hoangslevan@gmail.com>
2025-12-04 16:30:37 +0700

    Remove run_attack.py and test_brain.py; add rule_manager.py for rule management functionality

---
+++ src/blue_agent/rule_manager.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+RULE_FILE_PATH="configs/falco_rules.local.yaml"
+
+class RuleManager:
+    def add_rule(self, rule_yaml):
+        """
+        Docstring for add_rule
+        
+        them rule moi vao file local config
+        """
