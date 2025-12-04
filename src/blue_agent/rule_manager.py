4deb850fe82d06a18b49cf9695e83ad2a45a8d68
LeganSar44 <hoangslevan@gmail.com>
2025-12-04 16:34:57 +0700

    Refactor RuleManager class to improve rule addition and Falco reload functionality

--- src/blue_agent/rule_manager.py
+++ src/blue_agent/rule_manager.py
@@ -10,3 +10,21 @@ class RuleManager:
         
         them rule moi vao file local config
         """
+    # doc noi dung cu tranh trung lap
+    if not os.path.exists(RULE_FILE_PATH):
+        with open(RULE_FILE_PATH,'w') as f:
+            f.write ("# Auto-generated rules\n")
+
+    with open(RULE_FILE_PATH, 'a') as f:
+        f.write("\n"+ rule_yaml + "\n")
+
+    print(f"Rule added to {RULE_FILE_PATH}")
+
+
+    def reload_falco(self):
+        try:
+            subprocess.run(["docker", "restart", "falco"], check=True)
+            print("Falco restarted to apply new rules.")
+        except Exception as e:
+            print(f"Failed to reload Falco: {e}")
+            
